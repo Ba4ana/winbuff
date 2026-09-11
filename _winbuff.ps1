e5d5efa839f63acfa7e0fcfffa7b3e80b9c41af9
Ba4ana <dckwbacho@ya.ru>
2026-09-11 17:09:39 +0300

    v.0.6.5.2 7zip 2603

--- _winbuff.ps1
+++ _winbuff.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'SilentlyContinue'
 $ProgressPreference = 'SilentlyContinue'
 
 # Определение переменных
-$vers = "0.6.4"
+$vers = "0.6.5"
 $name = "_winbuff"
 $main = "$env:SystemRoot\$name"
 $logs = "$main\log"
@@ -62,8 +62,8 @@ if (!$brandmauer) {
 
 ## Установка 7-Zip
 # Версия 7-Zip
-$sevenZipDisplayVersion = "26.02"
-$sevenZipDownloadVersion = "2602"
+$sevenZipDisplayVersion = "26.03"
+$sevenZipDownloadVersion = "2603"
 
 # Определяем архитектуру ОС и соответствующий файл установщика
 $osArchitecture = (Get-WmiObject win32_operatingsystem).osarchitecture
